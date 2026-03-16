32fef2256f215683df8c88114d5bdf88ccfa779e
anormanangel <anormanangel@gmail.com>
2026-03-16 21:07:26 +0300

    Feat: Changed from UTC to EAT Time Zone

--- pipeline/01_ingest.py
+++ pipeline/01_ingest.py
@@ -1,9 +1,9 @@
 # Import Libraries
 import pandas as pd
 import os
-import shutil
 import logging
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
@@ -21,9 +21,10 @@ def ingest():
     df = pd.read_csv(SOURCE_FILE)
     logging.info(f"INGEST: {len(df)} rows read from '{SOURCE_FILE}'")
 
-    # Save to data lake with timestamp
+    # Save to data lake with timestamp in EAT
     os.makedirs(LAKE_PATH, exist_ok=True)
-    timestamp = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+    eat_time = datetime.now(ZoneInfo("Africa/Nairobi"))
+    timestamp = eat_time.strftime("%Y%m%d_%H%M%S")
     filename  = f"Input_{timestamp}.csv"
     filepath  = os.path.join(LAKE_PATH, filename)
 
